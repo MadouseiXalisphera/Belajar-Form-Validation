7d916272016d983d53dd4f9e606c62bd55e8fb87
MadouseiXalisphera <almulkanmarssya@gmail.com>
2025-12-20 11:10:10 +0700

    belajar form Validation day 1

---
+++ script.js
@@ -0,0 +1,43 @@
+// const input = document.querySelector("input");
+
+// input.addEventListener("input", (e) => {
+//    console.log(e.target.checkValidity());
+// })
+
+// input.addEventListener("input", (e) => {
+//   if (!e.target.checkValidity()) {
+//     e.target.reportValidity();
+//   }
+// })
+
+// input.addEventListener("input", (e) => {
+//   if (!e.target.checkValidity()) {
+//     e.target.setCustomValidity(
+//       "You must use a company email address that ends in @sampleCompany.com"
+//     );
+//   }
+// });
+
+// const pic = document.querySelector("input");
+
+// pic.addEventListener("input", (e) => {
+//   console.log(e.target.validity);
+// })
+
+//bagian ke 2
+
+
+// const input = document.querySelector("input");
+// const output = document.getElementById("output");
+
+// input.addEventListener("keydown", (e) => {
+//   output.innerText = `You pressed the ${e.key} key`;
+// });
+
+// const input = document.querySelector("input");
+// const output = document.getElementById("output");
+
+// input.addEventListener("keydown", (e) => {
+//       e.preventDefault();
+//   output.innerText = `You pressed the ${e.key} key`;
+// });

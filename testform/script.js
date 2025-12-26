711c0b6807ba8767db01a13751aed09169b70db6
MadouseiXalisphera <almulkanmarssya@gmail.com>
2025-12-26 17:57:14 +0700

    gw goblok, hampir gak belajar apa apa, jadi gw bikin lagi tuh form validation tapi custom.html css dlu, besok dah gw lanjutin js nya

---
+++ testform/script.js
@@ -0,0 +1,8 @@
+const form = document.querySelector("#mahasigma-form");
+
+const submitBtn = document.querySelector("#submit-button");
+const resetBtn = document.querySelector("#reset-button");
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+})
